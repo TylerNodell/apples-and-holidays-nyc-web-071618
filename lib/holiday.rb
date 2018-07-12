42e8cc91eff8f587a5f28eda88ec83ab26ee8e8f
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 15:42:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each { |season, data|
+    puts "#{season.capitalize}:"
+    data.each { |key, value|
+      puts "  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    }
+  }
 end
 
 def all_holidays_with_bbq(holiday_hash)
